82c99a360b4be8209fd693bf54fc1016eb05fcdd
thelilbeer <v.mikheev95@mail.ru>
2023-05-02 20:38:07 +0300

    Итоговое задание Модуль D6

--- NewsPortal/NewsPaper/news/templatetags/custom_filters.py
+++ NewsPortal/NewsPaper/news/templatetags/custom_filters.py
@@ -21,13 +21,11 @@ def currency(value, code='rub'):
    return f'{value} {postfix}'
 
 
-BANNED_WORDS = ['bad', 'words', 'to', 'censor']
+BANNED_WORDS = ['прыщ', 'редиска', 'кошак', 'шторм']
+
 
 @register.filter(name='censor')
 def censor(value):
-    """
-    A filter that censors the banned words in a string
-    """
     for word in BANNED_WORDS:
         value = value.replace(word, '*' * len(word))
     return value
